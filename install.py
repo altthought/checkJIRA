d55ed825a4fe7d9e253d141316fe4dae6d45e441
Alex Culp <aculp@marketo.com>
2018-08-28 11:11:42 -0700

    fixed installer

--- install.py
+++ install.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import subprocess
+import time
 import sys
 
+# version check
 if sys.version_info.major != 3:
     print('python3+ required')
 if sys.version_info.minor < 6:
     print('python3.6+ required')
-
 # install packages
-subprocess.run('python3 -m pip install -r requirements.txt')
-
+subprocess.run('python3 -m pip install -r requirements.txt'.split(' '))
