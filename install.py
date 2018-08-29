fad161c5d8fe945a872855dc36381de3a4050c8d
Alex Culp <aculp@marketo.com>
2018-08-29 09:29:43 -0700

    prevent python2 from executing/installing

--- install.py
+++ install.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 import subprocess
-import time
 import sys
 
 # version check
-if sys.version_info.major != 3:
-    print('python3+ required')
-if sys.version_info.minor < 6:
-    print('python3.6+ required')
+if sys.version_info.major < 3 or sys.version_info.minor < 6:
+    raise EnvironmentError('package requires python3.6+!')
 # install packages
 subprocess.run('python3 -m pip install -r requirements.txt'.split(' '))
