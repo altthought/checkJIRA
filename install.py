ca0bfc1e6106d131d33baa52dc940dffab679b76
Alex Culp <aculp@marketo.com>
2018-09-06 10:50:35 -0700

    we dont need an installer

--- install.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-import sys
-
-if sys.version_info.major < 3 or sys.version_info.minor < 6:
-    raise EnvironmentError('package requires python3.6+!')
-subprocess.run('python3 -m pip install -r requirements.txt'.split(' '))
