dbcf0b1f6c41a79064dc9c55c7398fc06c1c4e8d
Alex Culp <aculp@marketo.com>
2018-09-06 10:50:10 -0700

    useless comments

--- install.py
+++ install.py
@@ -2,8 +2,6 @@
 import subprocess
 import sys
 
-# version check
 if sys.version_info.major < 3 or sys.version_info.minor < 6:
     raise EnvironmentError('package requires python3.6+!')
-# install packages
 subprocess.run('python3 -m pip install -r requirements.txt'.split(' '))
